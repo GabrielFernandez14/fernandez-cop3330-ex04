bc0c9719d0f4198d605e580384dc6832ab253577
Gabriel Desktop <gabrielfernandez@knights.ucf.edu>
2021-05-30 17:51:27 -0400

    Updated spacing

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,27 +13,28 @@ public class App
     public static void main( String[] args )
     {
         // Prompt and scan in user's input for noun
-        System.out.println("Enter a noun: ");
+        System.out.println("Enter a noun:");
         Scanner nounInput = new Scanner(System.in);
         String noun = nounInput.next();
 
         // Prompt and scan in user's input for verb
-        System.out.println("Enter a verb: ");
+        System.out.println("Enter a verb:");
         Scanner verbInput = new Scanner(System.in);
         String verb = verbInput.next();
 
         // Prompt and scan in user's input for adjective
-        System.out.println("Enter an adjective: " );
+        System.out.println("Enter an adjective:" );
         Scanner adjectiveInput = new Scanner(System.in);
         String adjective = adjectiveInput.next();
 
         // Prompt and scan in user's input for adverb
-        System.out.println("Enter an adverb: ");
+        System.out.println("Enter an adverb:");
         Scanner adverbInput = new Scanner(System.in);
         String adverb = adverbInput.next();
 
         // Print out complete mad lib using string interpolation
-        String mad_lib = String.format("Do you %s your %s %s %s? That's hilarious!", verb, adjective, noun, adverb);
+        String mad_lib = String.format("Do you %s your %s %s %s? " +
+                "That's hilarious!", verb, adjective, noun, adverb);
         System.out.println(mad_lib);
     }
 }
